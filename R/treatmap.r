d9f90b032e93d5ce370d041a679e8c317b1b7a99
cmfield <fieldc@ethz.ch>
2023-08-09 16:42:58 +0200

    how did I not notice these bugs

--- R/treatmap.r
+++ R/treatmap.r
@@ -147,7 +147,7 @@ treatmap <- function(phylo, mat, mask=NULL, mask.color="lightgrey", overlay=NULL
     mat.colors = z.cols[cells]
     mat.colors[!mask] = mask.color
     rect(rep(phyloWidth+tip.label.width+(0:(ncol(mat)-1)), each=nrow(mat)), 0.5+rep(0:(nrow(mat)-1), ncol(mat)), rep(phyloWidth+tip.label.width+(1:ncol(mat)), each=nrow(mat)), 0.5+rep(1:nrow(mat), ncol(mat)), col=mat.colors, border=NA)
-    text(phyloWidth+tip.label.width+(0.5+0:(ncol(mat)-1)), nrow(mat)+1, mat.labels[hc$order], adj=c(0, 0.5), srt=90, col=mat.label.colors[hc$order])
+    text(phyloWidth+tip.label.width+(0.5+0:(ncol(mat)-1)), nrow(mat)+1, mat.labels, adj=c(0, 0.5), srt=90, col=mat.label.colors[hc$order])
     # Additional borders added after for correct overlap
     rect(rep(phyloWidth+tip.label.width+(0:(ncol(mat)-1)), each=nrow(mat)), 0.5+rep(0:(nrow(mat)-1), ncol(mat)), rep(phyloWidth+tip.label.width+(1:ncol(mat)), each=nrow(mat)), 0.5+rep(1:nrow(mat), ncol(mat)), col=NA, border=borders.col, lty=borders.lty, lwd=borders.lwd)
 
